7ad54622be050229c24d066aed1567e88df77978
Chideraa <chideraaozobu@gmail.com>
2024-05-26 11:09:59 +0200

    cache test

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Create a class BasicCache that inherits from BaseCaching and is a caching system:
+""" Create a class BasicCache that inherits from
+    BaseCaching and is a caching system:
 """
 from base_caching import BaseCaching
 
@@ -9,14 +10,14 @@ class BasicCache(BaseCaching):
     BaseCaching. This caching system doesn’t have limit"""
 
     def put(self, key, item):
-        """Must assign to the dictionary self.cache_data 
+        """Must assign to the dictionary self.cache_data
         the item value for the key key.
 
         If key or item is None, this method should not do anything."""
         if key is None or item is None:
             return
         self.cache_data[key] = item
-    
+
     def get(self, key):
         """Must return the value in self.cache_data linked to key.
         If key is None or if the key doesn’t exist in self.cache_data,
